b9c3ed8465553bc23a7186b71fc6217698b23a0c
bkm <23>
2022-09-20 11:42:29 +1000

    environment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ov7sfl!n!$3hvtqu)c2-uesn&me*l=edlodj*z8=%qm(0pnu-x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
